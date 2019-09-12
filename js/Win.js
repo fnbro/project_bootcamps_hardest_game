ad2cbb144e78f411433fd3a437a39ac914ff650d
Nestler, Florian <nestlerf@mediamarktsaturn.com>
2019-09-12 11:29:22 +0200

    updated the goal

--- js/Win.js
+++ js/Win.js
@@ -7,5 +7,11 @@ class Win {
         this.context.font = "30px Arial";
         this.context.fillText("Goal", 730, 370);
         this.context.fill();
+
+        this.context.save();
+        this.context.fillStyle = "rgba(0, 102, 34, 0.5)"
+        this.context.fillRect(720, 320, 120, 80);
+        this.context.restore();
+        
     }
 }
